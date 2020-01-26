db61ddbc0cd694c967d106d70eab3e189f85dec7
ashna shah <a5hna@hotmail.co.uk>
2020-01-26 16:07:49 +0000

    ammeded models and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'outfits#index'
+
+  resources :outfits, only: [:index, :show] do
+    resources :reviews, only: [:create]
+    resources :favourites, only: [:create, :destroy]
+    resources :wardrobe_items, only: [:create]
+  end
+  resources :wardrobes, only: [:show] do
+    resources :wardrobe_items, only: [:edit, :update, :destroy]
+    resources :orders, only: [:new]
+    resources :order_items, only: [:new]
+  end
+  resources :favourites, only: [:show]
+  resources :orders, only: [:create]
+  resources :order_items, only: [:create]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
